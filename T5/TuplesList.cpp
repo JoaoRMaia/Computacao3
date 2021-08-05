6591a93436f402fea0a22a66ae6658f60bf85597
JoaoRMaia <jrcampbellmaia@gmail.com>
2021-08-04 21:17:07 -0300

    100/100

--- T5/TuplesList.cpp
+++ T5/TuplesList.cpp
@@ -3,47 +3,45 @@
 
 using namespace std;
 
-struct AbstractPair;
-
-struct ImplrPair{
-	
-}
-
-class Pair {
-public:
-  template <typename A, typename B>
-  Pair( A a, B b ) {
-	  cout << a << b << endl;
-	  p = new AbstractPair<A,B>(a,b);
-	  
-    
-  }
-private:
-  AbstractPair *p;
+struct AbstractPair {	
+	virtual void print( ostream& o ){}
 };
 
-struct AbstractPair : public Pair {
+template <typename A, typename B>
+struct ImplPair : AbstractPair {
+	ImplPair(A a, B b) : a(a), b(b) {}
 	
-	template<typename A, typename B>
-	AbstractPair ( A a, B b){
-		cout << a << b << endl;
-		
+	void print( ostream& o ) {
+		o << a << " = " << b << endl;
 	}
 	
+private:
 	A a;
 	B b;
 	
-	
-	
 };
 
+struct Pair {
+  template <typename A, typename B>
+  Pair( A a, B b ) {
+	  p = new ImplPair<A,B>(a,b);
+	  
+  }
+  
+  AbstractPair *p;
+};
+
+
 void print( ostream& o, initializer_list<Pair> lista ) {
+	for (auto e : lista){
+		e.p->print(o);
 	}
+}
 
 int main() {
  
     Pair p( "1", 2 );
-    cout << typeid(p).name();
+    cout << typeid(p).name() << endl;
   
     print( cout, { { "jan", 1 }, { 2, "fev" }, { string( "pi" ), 3.14 } } );
 
