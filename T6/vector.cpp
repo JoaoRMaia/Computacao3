42af9d4379b5c0a18932e4d5b92f47c00a9758fa
JoaoRMaia <jrcampbellmaia@gmail.com>
2021-08-11 16:49:19 -0300

    Produto Escalar

--- T6/vector.cpp
+++ T6/vector.cpp
@@ -5,61 +5,79 @@ using namespace std;
 
 template<int n, typename T>
 struct Vetor {
-	struct MeioOp{
-		MeioOp( Vetor* v ): u(v) {}
-		
-		MeioOp operator , ( T a ){
-			*u = n;
-			return *this;
-		}
-		Vetor 
-		
-	};
-	
-	Vetor(){
-	}
-	Vetor(initializer_list<T> v){
-		
-		
-	}
-	void operator =( initializer_list<T> v ) {
-		
-	}
-	
-	MeioOp operator = ( T a ) {
-		u.push_back(a);
-		return MeioOp(this);
-	}
-		
-	vector<T> u;
-	
+	Vetor(){}
 
-	
-	
+	struct MeioOP{
+	    MeioOP(vector<T> u) : v(u) {}
+
+	    vector<T> v;
+	};
+
+
+	Vetor& operator =(T e) {
+	    u.push_back(e);
+	    return *this;
+	}
+
+	Vetor& operator ,(T e) {
+	    u.push_back(e);
+	    return *this;
+	}
+	Vetor operator +(Vetor v) {
+	    Vetor<n,T> r;
+	    for (int i = 0 ; i < n ; i++){
+            r[i] = this[i] + v[i];
+	    }
+
+	    return r;
+	}
+	Vetor operator *(T v) {
+	    Vetor<n,T> r;
+	    for (int i = 0 ; i < n ; i++){
+            r[i] = this[i] + v[i];
+	    }
+
+	    return r;
+	}
+
+	T operator *(Vetor<n,T> v){
+	    T sum = 0;
+	    for (int i = 0 ; i < n ; i++){
+            sum += u.at(i)*v.u.at(i);
+	    }
+	    return sum;
+
+	}
+
+
+
+	MeioOP operator* (){
+	    return MeioOP(u);
+	}
+	Vetor<n,T> operator *(MeioOP v){
+	    return *this;
+	}
+
+
+	vector<T> u;
 };
 
-template<typename T>
-initializer_list<T>& operator ,(T a, T b){
-	return {a,b};
-	
+template <int n, typename T>
+ostream& operator << (ostream& o, Vetor<n,T> v) {
+    return o;
 }
-
-template<typename T>
-initializer_list<T>& operator ,(initializer_list<T>& a, T b){
-	return {*a.begin(),*a.end(),b};
-	
-}
-
 
 
 
 #ifdef NDEBUG
 
 int main () {
-	
-	Vetor <2,int> a;
+
+	Vetor <2,double> a;
 	Vetor<2,double> v;
 	v = 4.5, 6.1;
+	a = 5.5, 10.0;
+	cout << a*v;
 	return 0;
 }
 
